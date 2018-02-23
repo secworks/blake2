dea3e2c7a8db47eac84704de79a62db3e985601c
Joachim Strömbergson <joachim@secworks.se>
2018-02-23 14:51:53 +0100

    Reworked the code to use arrays of regs. Cleaned up the code.

--- src/rtl/blake2_core.v
+++ src/rtl/blake2_core.v
@@ -127,57 +127,13 @@ module blake2_core(
   //----------------------------------------------------------------
   // Registers including update variables and write enable.
   //----------------------------------------------------------------
-  reg [63 : 0] h0_reg;
-  reg [63 : 0] h0_new;
-  reg [63 : 0] h1_reg;
-  reg [63 : 0] h1_new;
-  reg [63 : 0] h2_reg;
-  reg [63 : 0] h2_new;
-  reg [63 : 0] h3_reg;
-  reg [63 : 0] h3_new;
-  reg [63 : 0] h4_reg;
-  reg [63 : 0] h4_new;
-  reg [63 : 0] h5_reg;
-  reg [63 : 0] h5_new;
-  reg [63 : 0] h6_reg;
-  reg [63 : 0] h6_new;
-  reg [63 : 0] h7_reg;
-  reg [63 : 0] h7_new;
-  reg          update_chain_value;
+  reg [63 : 0] h_reg [0 : 7];
+  reg [63 : 0] h_new [0 : 7];
   reg          h_we;
+  reg          update_chain_value;
 
-  reg [63 : 0] v0_reg;
-  reg [63 : 0] v0_new;
-  reg [63 : 0] v1_reg;
-  reg [63 : 0] v1_new;
-  reg [63 : 0] v2_reg;
-  reg [63 : 0] v2_new;
-  reg [63 : 0] v3_reg;
-  reg [63 : 0] v3_new;
-  reg [63 : 0] v4_reg;
-  reg [63 : 0] v4_new;
-  reg [63 : 0] v5_reg;
-  reg [63 : 0] v5_new;
-  reg [63 : 0] v6_reg;
-  reg [63 : 0] v6_new;
-  reg [63 : 0] v7_reg;
-  reg [63 : 0] v7_new;
-  reg [63 : 0] v8_reg;
-  reg [63 : 0] v8_new;
-  reg [63 : 0] v9_reg;
-  reg [63 : 0] v9_new;
-  reg [63 : 0] v10_reg;
-  reg [63 : 0] v10_new;
-  reg [63 : 0] v11_reg;
-  reg [63 : 0] v11_new;
-  reg [63 : 0] v12_reg;
-  reg [63 : 0] v12_new;
-  reg [63 : 0] v13_reg;
-  reg [63 : 0] v13_new;
-  reg [63 : 0] v14_reg;
-  reg [63 : 0] v14_new;
-  reg [63 : 0] v15_reg;
-  reg [63 : 0] v15_new;
+  reg [31 : 0] v_reg [0 : 15];
+  reg [31 : 0] v_new [0 : 15];
   reg          v_we;
 
   reg [63 : 0] f1_reg;
@@ -352,22 +308,22 @@ module blake2_core(
   //----------------------------------------------------------------
   // Concurrent connectivity for ports etc.
   //----------------------------------------------------------------
-  assign digest = {h0_reg[7:0],   h0_reg[15:8],  h0_reg[23:16], h0_reg[31:24],
-                   h0_reg[39:32], h0_reg[47:40], h0_reg[55:48], h0_reg[63:56],
-                   h1_reg[7:0],   h1_reg[15:8],  h1_reg[23:16], h1_reg[31:24],
-                   h1_reg[39:32], h1_reg[47:40], h1_reg[55:48], h1_reg[63:56],
-                   h2_reg[7:0],   h2_reg[15:8],  h2_reg[23:16], h2_reg[31:24],
-                   h2_reg[39:32], h2_reg[47:40], h2_reg[55:48], h2_reg[63:56],
-                   h3_reg[7:0],   h3_reg[15:8],  h3_reg[23:16], h3_reg[31:24],
-                   h3_reg[39:32], h3_reg[47:40], h3_reg[55:48], h3_reg[63:56],
-                   h4_reg[7:0],   h4_reg[15:8],  h4_reg[23:16], h4_reg[31:24],
-                   h4_reg[39:32], h4_reg[47:40], h4_reg[55:48], h4_reg[63:56],
-                   h5_reg[7:0],   h5_reg[15:8],  h5_reg[23:16], h5_reg[31:24],
-                   h5_reg[39:32], h5_reg[47:40], h5_reg[55:48], h5_reg[63:56],
-                   h6_reg[7:0],   h6_reg[15:8],  h6_reg[23:16], h6_reg[31:24],
-                   h6_reg[39:32], h6_reg[47:40], h6_reg[55:48], h6_reg[63:56],
-                   h7_reg[7:0],   h7_reg[15:8],  h7_reg[23:16], h7_reg[31:24],
-                   h7_reg[39:32], h7_reg[47:40], h7_reg[55:48], h7_reg[63:56]};
+  assign digest = {h_reg[0][7:0],   h_reg[0][15:8],  h_reg[0][23:16], h_reg[0][31:24],
+                   h_reg[0][39:32], h_reg[0][47:40], h_reg[0][55:48], h_reg[0][63:56],
+                   h_reg[1][7:0],   h_reg[1][15:8],  h_reg[1][23:16], h_reg[1][31:24],
+                   h_reg[1][39:32], h_reg[1][47:40], h_reg[1][55:48], h_reg[1][63:56],
+                   h_reg[2][7:0],   h_reg[2][15:8],  h_reg[2][23:16], h_reg[2][31:24],
+                   h_reg[2][39:32], h_reg[2][47:40], h_reg[2][55:48], h_reg[2][63:56],
+                   h_reg[3][7:0],   h_reg[3][15:8],  h_reg[3][23:16], h_reg[3][31:24],
+                   h_reg[3][39:32], h_reg[3][47:40], h_reg[3][55:48], h_reg[3][63:56],
+                   h_reg[4][7:0],   h_reg[4][15:8],  h_reg[4][23:16], h_reg[4][31:24],
+                   h_reg[4][39:32], h_reg[4][47:40], h_reg[4][55:48], h_reg[4][63:56],
+                   h_reg[5][7:0],   h_reg[5][15:8],  h_reg[5][23:16], h_reg[5][31:24],
+                   h_reg[5][39:32], h_reg[5][47:40], h_reg[5][55:48], h_reg[5][63:56],
+                   h_reg[6][7:0],   h_reg[6][15:8],  h_reg[6][23:16], h_reg[6][31:24],
+                   h_reg[6][39:32], h_reg[6][47:40], h_reg[6][55:48], h_reg[6][63:56],
+                   h_reg[7][7:0],   h_reg[7][15:8],  h_reg[7][23:16], h_reg[7][31:24],
+                   h_reg[7][39:32], h_reg[7][47:40], h_reg[7][55:48], h_reg[7][63:56]};
 
   assign digest_valid = digest_valid_reg;
 
@@ -384,33 +340,17 @@ module blake2_core(
   //----------------------------------------------------------------
   always @ (posedge clk)
     begin : reg_update
+      integer i;
+
       if (!reset_n)
         begin
+          for (i = 0; i < 8; i = i + 1)
+            h_reg[i] <= 32'h0;
+
+          for (i = 0; i < 16; i = i + 1)
+            v_reg[i] <= 32'h0;
+
           f1_reg             <= 64'h0;
-          h0_reg             <= 64'h0;
-          h1_reg             <= 64'h0;
-          h2_reg             <= 64'h0;
-          h3_reg             <= 64'h0;
-          h4_reg             <= 64'h0;
-          h5_reg             <= 64'h0;
-          h6_reg             <= 64'h0;
-          h7_reg             <= 64'h0;
-          v0_reg             <= 64'h0;
-          v1_reg             <= 64'h0;
-          v2_reg             <= 64'h0;
-          v3_reg             <= 64'h0;
-          v4_reg             <= 64'h0;
-          v5_reg             <= 64'h0;
-          v6_reg             <= 64'h0;
-          v7_reg             <= 64'h0;
-          v8_reg             <= 64'h0;
-          v9_reg             <= 64'h0;
-          v10_reg            <= 64'h0;
-          v11_reg            <= 64'h0;
-          v12_reg            <= 64'h0;
-          v13_reg            <= 64'h0;
-          v14_reg            <= 64'h0;
-          v15_reg            <= 64'h0;
           ready_reg          <= 1;
           digest_valid_reg   <= 0;
           G_ctr_reg          <= STATE_G0;
@@ -421,34 +361,14 @@ module blake2_core(
         begin
           if (h_we)
             begin
-              h0_reg  <= h0_new;
-              h1_reg  <= h1_new;
-              h2_reg  <= h2_new;
-              h3_reg  <= h3_new;
-              h4_reg  <= h4_new;
-              h5_reg  <= h5_new;
-              h6_reg  <= h6_new;
-              h7_reg  <= h7_new;
+              for (i = 0; i < 8; i = i + 1)
+                h_reg[i] <= h_new[i];
             end
 
           if (v_we)
             begin
-              v0_reg  <= v0_new;
-              v1_reg  <= v1_new;
-              v2_reg  <= v2_new;
-              v3_reg  <= v3_new;
-              v4_reg  <= v4_new;
-              v5_reg  <= v5_new;
-              v6_reg  <= v6_new;
-              v7_reg  <= v7_new;
-              v8_reg  <= v8_new;
-              v9_reg  <= v9_new;
-              v10_reg <= v10_new;
-              v11_reg <= v11_new;
-              v12_reg <= v12_new;
-              v13_reg <= v13_new;
-              v14_reg <= v14_new;
-              v15_reg <= v15_new;
+              for (i = 0; i < 8; i = i + 1)
+                v_reg[i] <= v_new[i];
             end
 
           if (ready_we)
@@ -486,41 +406,36 @@ module blake2_core(
   //----------------------------------------------------------------
   always @*
     begin : chain_logic
+      integer i;
+
+      for (i = 0; i < 8; i = i + 1)
+        h_new[i] = 64'h0;
+      h_we   = 0;
+
       if (init_state)
         begin
-          h0_new  = IV0 ^ parameter_block[63:0];
-          h1_new  = IV1 ^ parameter_block[127:64];
-          h2_new  = IV2 ^ parameter_block[191:128];
-          h3_new  = IV3 ^ parameter_block[255:192];
-          h4_new  = IV4 ^ parameter_block[319:256];
-          h5_new  = IV5 ^ parameter_block[383:320];
-          h6_new  = IV6 ^ parameter_block[447:384];
-          h7_new  = IV7 ^ parameter_block[511:448];
+          h_new[0]  = IV0 ^ parameter_block[63  :   0];
+          h_new[1]  = IV1 ^ parameter_block[127 :  64];
+          h_new[2]  = IV2 ^ parameter_block[191 : 128];
+          h_new[3]  = IV3 ^ parameter_block[255 : 192];
+          h_new[4]  = IV4 ^ parameter_block[319 : 256];
+          h_new[5]  = IV5 ^ parameter_block[383 : 320];
+          h_new[6]  = IV6 ^ parameter_block[447 : 384];
+          h_new[7]  = IV7 ^ parameter_block[511 : 448];
           h_we    = 1;
         end
-      else if (update_chain_value)
+
+      if (update_chain_value)
         begin
-          h0_new = h0_reg ^ v0_reg ^ v8_reg;
-          h1_new = h1_reg ^ v1_reg ^ v9_reg;
-          h2_new = h2_reg ^ v2_reg ^ v10_reg;
-          h3_new = h3_reg ^ v3_reg ^ v11_reg;
-          h4_new = h4_reg ^ v4_reg ^ v12_reg;
-          h5_new = h5_reg ^ v5_reg ^ v13_reg;
-          h6_new = h6_reg ^ v6_reg ^ v14_reg;
-          h7_new = h7_reg ^ v7_reg ^ v15_reg;
+          h_new[0] = h_reg[0] ^ v_reg[0] ^ v_reg[8];
+          h_new[1] = h_reg[1] ^ v_reg[1] ^ v_reg[9];
+          h_new[2] = h_reg[2] ^ v_reg[2] ^ v_reg[10];
+          h_new[3] = h_reg[3] ^ v_reg[3] ^ v_reg[11];
+          h_new[4] = h_reg[4] ^ v_reg[4] ^ v_reg[12];
+          h_new[5] = h_reg[5] ^ v_reg[5] ^ v_reg[13];
+          h_new[6] = h_reg[6] ^ v_reg[6] ^ v_reg[14];
+          h_new[7] = h_reg[7] ^ v_reg[7] ^ v_reg[15];
           h_we   = 1;
-        end
-      else
-        begin
-          h0_new = 64'h0;
-          h1_new = 64'h0;
-          h2_new = 64'h0;
-          h3_new = 64'h0;
-          h4_new = 64'h0;
-          h5_new = 64'h0;
-          h6_new = 64'h0;
-          h7_new = 64'h0;
-          h_we   = 0;
         end
     end // chain_logic
 
@@ -532,162 +447,141 @@ module blake2_core(
   //----------------------------------------------------------------
   always @*
     begin : state_logic
+      integer i;
+
+      for (i = 0; i < 16; i = i + 1)
+        v_new[i] = 64'h0;
+      v_we = 0;
+
+      G0_a = 64'h0;
+      G0_b = 64'h0;
+      G0_c = 64'h0;
+      G0_d = 64'h0;
+      G1_a = 64'h0;
+      G1_b = 64'h0;
+      G1_c = 64'h0;
+      G1_d = 64'h0;
+      G2_a = 64'h0;
+      G2_b = 64'h0;
+      G2_c = 64'h0;
+      G2_d = 64'h0;
+      G3_a = 64'h0;
+      G3_b = 64'h0;
+      G3_c = 64'h0;
+      G3_d = 64'h0;
+
       if (init_state)
         begin
-          v0_new  = IV0 ^ parameter_block[63:0];
-          v1_new  = IV1 ^ parameter_block[127:64];
-          v2_new  = IV2 ^ parameter_block[191:128];
-          v3_new  = IV3 ^ parameter_block[255:192];
-          v4_new  = IV4 ^ parameter_block[319:256];
-          v5_new  = IV5 ^ parameter_block[383:320];
-          v6_new  = IV6 ^ parameter_block[447:384];
-          v7_new  = IV7 ^ parameter_block[511:448];
-          v8_new  = IV0;
-          v9_new  = IV1;
-          v10_new = IV2;
-          v11_new = IV3;
-          v12_new = data_length[63:0] ^ IV4;
-          v13_new = t1_reg ^ IV5;
-
-          if (final_block)
-            v14_new = 64'hffffffffffffffff ^ IV6;
-          else
-            v14_new = IV6;
-
-          v15_new = f1_reg ^ IV7;
+          v_new[0]  = IV0 ^ parameter_block[63:0];
+          v_new[1]  = IV1 ^ parameter_block[127:64];
+          v_new[2]  = IV2 ^ parameter_block[191:128];
+          v_new[3]  = IV3 ^ parameter_block[255:192];
+          v_new[4]  = IV4 ^ parameter_block[319:256];
+          v_new[5]  = IV5 ^ parameter_block[383:320];
+          v_new[6]  = IV6 ^ parameter_block[447:384];
+          v_new[7]  = IV7 ^ parameter_block[511:448];
+          v_new[8]  = IV0;
+          v_new[9]  = IV1;
+          v_new[10] = IV2;
+          v_new[11] = IV3;
+          v_new[12] = data_length[63:0] ^ IV4;
+          v_new[13] = t1_reg ^ IV5;
+          v_new[14] = {IV6 ^ {64{final_block}}};
+          v_new[15] = f1_reg ^ IV7;
           v_we    = 1;
         end
+
       else if (update_state)
         begin
+          v_we    = 1;
+
           case (G_ctr_reg)
             // Column updates.
             STATE_G0:
               begin
-                G0_a    = v0_reg;
-                G0_b    = v4_reg;
-                G0_c    = v8_reg;
-                G0_d    = v12_reg;
-                v0_new  = G0_a_prim;
-                v4_new  = G0_b_prim;
-                v8_new  = G0_c_prim;
-                v12_new = G0_d_prim;
+                G0_a      = v_reg[0];
+                G0_b      = v_reg[4];
+                G0_c      = v_reg[8];
+                G0_d      = v_reg[12];
+                v_new[0]  = G0_a_prim;
+                v_new[4]  = G0_b_prim;
+                v_new[8]  = G0_c_prim;
+                v_new[12] = G0_d_prim;
 
-                G1_a    = v1_reg;
-                G1_b    = v5_reg;
-                G1_c    = v9_reg;
-                G1_d    = v13_reg;
-                v1_new  = G1_a_prim;
-                v5_new  = G1_b_prim;
-                v9_new  = G1_c_prim;
-                v13_new = G1_d_prim;
+                G1_a      = v_reg[1];
+                G1_b      = v_reg[5];
+                G1_c      = v_reg[9];
+                G1_d      = v_reg[13];
+                v_new[1]  = G1_a_prim;
+                v_new[5]  = G1_b_prim;
+                v_new[9]  = G1_c_prim;
+                v_new[13] = G1_d_prim;
 
-                G2_a    = v2_reg;
-                G2_b    = v6_reg;
-                G2_c    = v10_reg;
-                G2_d    = v14_reg;
-                v2_new  = G2_a_prim;
-                v6_new  = G2_b_prim;
-                v10_new = G2_c_prim;
-                v14_new = G2_d_prim;
+                G2_a      = v_reg[2];
+                G2_b      = v_reg[6];
+                G2_c      = v_reg[10];
+                G2_d      = v_reg[14];
+                v_new[2]  = G2_a_prim;
+                v_new[6]  = G2_b_prim;
+                v_new[10] = G2_c_prim;
+                v_new[14] = G2_d_prim;
 
-                G3_a    = v3_reg;
-                G3_b    = v7_reg;
-                G3_c    = v11_reg;
-                G3_d    = v15_reg;
-                v3_new  = G3_a_prim;
-                v7_new  = G3_b_prim;
-                v11_new = G3_c_prim;
-                v15_new = G3_d_prim;
-
-                v_we    = 1;
+                G3_a      = v_reg[3];
+                G3_b      = v_reg[7];
+                G3_c      = v_reg[11];
+                G3_d      = v_reg[15];
+                v_new[3]  = G3_a_prim;
+                v_new[7]  = G3_b_prim;
+                v_new[11] = G3_c_prim;
+                v_new[15] = G3_d_prim;
+                v_we = 1;
               end
 
             // Diagonal updates.
             STATE_G1:
               begin
-                G0_a    = v0_reg;
-                G0_b    = v5_reg;
-                G0_c    = v10_reg;
-                G0_d    = v15_reg;
-                v0_new  = G0_a_prim;
-                v5_new  = G0_b_prim;
-                v10_new = G0_c_prim;
-                v15_new = G0_d_prim;
+                G0_a      = v_reg[0];
+                G0_b      = v_reg[5];
+                G0_c      = v_reg[10];
+                G0_d      = v_reg[15];
+                v_new[0]  = G0_a_prim;
+                v_new[5]  = G0_b_prim;
+                v_new[10] = G0_c_prim;
+                v_new[15] = G0_d_prim;
 
-                G1_a    = v1_reg;
-                G1_b    = v6_reg;
-                G1_c    = v11_reg;
-                G1_d    = v12_reg;
-                v1_new  = G1_a_prim;
-                v6_new  = G1_b_prim;
-                v11_new = G1_c_prim;
-                v12_new = G1_d_prim;
+                G1_a      = v_reg[1];
+                G1_b      = v_reg[6];
+                G1_c      = v_reg[11];
+                G1_d      = v_reg[12];
+                v_new[1]  = G1_a_prim;
+                v_new[6]  = G1_b_prim;
+                v_new[11] = G1_c_prim;
+                v_new[12] = G1_d_prim;
 
-                G2_a    = v2_reg;
-                G2_b    = v7_reg;
-                G2_c    = v8_reg;
-                G2_d    = v13_reg;
-                v2_new  = G2_a_prim;
-                v7_new  = G2_b_prim;
-                v8_new  = G2_c_prim;
-                v13_new = G2_d_prim;
+                G2_a      = v_reg[2];
+                G2_b      = v_reg[7];
+                G2_c      = v_reg[8];
+                G2_d      = v_reg[13];
+                v_new[2]  = G2_a_prim;
+                v_new[7]  = G2_b_prim;
+                v_new[8]  = G2_c_prim;
+                v_new[13] = G2_d_prim;
 
-                G3_a    = v3_reg;
-                G3_b    = v4_reg;
-                G3_c    = v9_reg;
-                G3_d    = v14_reg;
-                v3_new  = G3_a_prim;
-                v4_new  = G3_b_prim;
-                v9_new  = G3_c_prim;
-                v14_new = G3_d_prim;
-
-                v_we    = 1;
+                G3_a      = v_reg[3];
+                G3_b      = v_reg[4];
+                G3_c      = v_reg[9];
+                G3_d      = v_reg[14];
+                v_new[3]  = G3_a_prim;
+                v_new[4]  = G3_b_prim;
+                v_new[9]  = G3_c_prim;
+                v_new[14] = G3_d_prim;
               end
 
             default:
               begin
-                G0_a = 0;
-                G0_b = 0;
-                G0_c = 0;
-                G0_d = 0;
-
-                G1_a = 0;
-                G1_b = 0;
-                G1_c = 0;
-                G1_d = 0;
-
-                G2_a = 0;
-                G2_b = 0;
-                G2_c = 0;
-                G2_d = 0;
-
-                G3_a = 0;
-                G3_b = 0;
-                G3_c = 0;
-                G3_d = 0;
               end
           endcase // case (G_ctr_reg)
         end // if (update_state)
-      else
-        begin
-          v0_new  = 64'h0;
-          v1_new  = 64'h0;
-          v2_new  = 64'h0;
-          v3_new  = 64'h0;
-          v4_new  = 64'h0;
-          v5_new  = 64'h0;
-          v6_new  = 64'h0;
-          v7_new  = 64'h0;
-          v8_new  = 64'h0;
-          v9_new  = 64'h0;
-          v10_new = 64'h0;
-          v11_new = 64'h0;
-          v12_new = 64'h0;
-          v13_new = 64'h0;
-          v14_new = 64'h0;
-          v15_new = 64'h0;
-          v_we    = 0;
-        end
     end // state_logic
 
 
