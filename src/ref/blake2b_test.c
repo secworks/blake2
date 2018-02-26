0cad0707021095b30596bc0846a50c35d19953c4
Joachim Strömbergson <joachim@secworks.se>
2018-02-26 14:10:05 +0100

    Adding self testing test case of appenfix A.

--- src/ref/blake2b_test.c
+++ src/ref/blake2b_test.c
@@ -132,7 +132,50 @@ static void self_test()
 //------------------------------------------------------------------
 static void rfc_test()
 {
-  printf("Running the test case from Appendix A\n");
+  uint8_t expected[64] = {0xba, 0x80, 0xa5, 0x3f, 0x98, 0x1c, 0x4d, 0x0d,
+                          0x6a, 0x27, 0x97, 0xb6, 0x9f, 0x12, 0xf6, 0xe9,
+                          0x4c, 0x21, 0x2f, 0x14, 0x68, 0x5a, 0xc4, 0xb7,
+                          0x4b, 0x12, 0xbb, 0x6f, 0xdb, 0xff, 0xa2, 0xd1,
+                          0x7d, 0x87, 0xc5, 0x39, 0x2a, 0xab, 0x79, 0x2d,
+                          0xc2, 0x52, 0xd5, 0xde, 0x45, 0x33, 0xcc, 0x95,
+                          0x18, 0xd3, 0x8a, 0xa8, 0xdb, 0xf1, 0x92, 0x5a,
+                          0xb9, 0x23, 0x86, 0xed, 0xd4, 0x00, 0x99, 0x23};
+
+  uint8_t digest[64];
+  uint8_t message[3] = "abc";
+  uint8_t i;
+  uint8_t errors = 0;
+
+  printf("Running the blake2b-512 test case from Appendix A\n");
+
+  blake2b(&digest[0], 64, 0, 0, &message[0], 3);
+
+  for (i = 0 ; i < 64 ; i++)
+    if (digest[i] != expected[i])
+      errors += 1;
+
+  if (errors == 0) {
+    printf("Correct digest generated.\n");
+  }
+
+  else {
+    printf("%d errors generated.\n", errors);
+    printf("Expected:\n");
+    for (i = 0; i < 64 ; i++) {
+      if ((i > 0) && (i % 16 == 0))
+        printf("\n");
+      printf("0x%02x ", expected[i]);
+    }
+    printf("\n");
+
+    printf("Generated:\n");
+    for (i = 0; i < 64 ; i++) {
+      if ((i > 0) && (i % 16 == 0))
+        printf("\n");
+      printf("0x%02x ", digest[i]);
+    }
+    printf("\n");
+  }
 }
 
 
